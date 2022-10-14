1f2845df1e0e459d1bf321486cd84a390925934f
Agahan14 <imamidinov.agahan06@gmail.com>
2022-10-14 17:15:51 +0600

    add url to allowed_host

--- rentit/settings.py
+++ rentit/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')  # 'django-insecure-w=$38q@9d9c+z*0*5$)+is&-s%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'neorentit.herokuapp.com']
 
 
 # Application definition
