5cac052d416e475a68c6a3d5e719906be963337a
agahan14 <imamidinov.agahan06@gmail.com>
2022-12-14 23:33:46 +0600

    added chat application

--- chat/models.py
+++ chat/models.py
@@ -11,6 +11,11 @@ class Contact(models.Model):
     friends = models.ManyToManyField('self', blank=True)
 
     def __str__(self):
+        if self.user.pictures == "":
+            return '{ user_id: ' + str(self.user.id) + \
+                   ', first_name: ' + self.user.first_name + \
+                   ', last_name: ' + self.user.last_name + \
+                   ', pictures: null' + '}'
 
         return '{ user_id: ' + str(self.user.id) + \
                ', first_name: ' + self.user.first_name + \
